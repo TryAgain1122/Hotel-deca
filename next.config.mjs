8c9ece1dddfcb6de46acbb69d9314540bb34ebea
TryAgain1122 <luisrafhael1122@gmail.com>
2024-06-01 07:21:57 +0800

    change commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["lh3.googleusercontent.com", "images.unsplash.com", "scontent.fmnl4-3.fna.fbcdn.net", "scontent.fmnl4-1.fna.fbcdn.net", "scontent.fmnl4-6.fna.fbcdn.net", "mega.nz", "scontent.fmnl4-4.fna.fbcdn.net"],
+        domains: ["lh3.googleusercontent.com", "images.unsplash.com", "scontent.fmnl4-3.fna.fbcdn.net", "scontent.fmnl4-1.fna.fbcdn.net", "scontent.fmnl4-6.fna.fbcdn.net", "mega.nz", "scontent.fmnl4-4.fna.fbcdn.net", "cdn.sanity.io", "scontent.xx.fbcdn.net", ],
     }
 };
 
--- src/app/components/FeaturedRoom.tsx/FeaturedRoom.tsx
+++ src/app/components/FeaturedRoom.tsx/FeaturedRoom.tsx
@@ -1,20 +1,23 @@
-import { Room } from '@/models/room'
+'use client';
+
+import { FC } from 'react';
 import Image from 'next/image';
+
+import { Room } from '@/models/room';
 import Link from 'next/link';
-import React, { FC } from 'react'
-import { TbCurrencyPeso } from "react-icons/tb";
 
-
-type Props =  {
+type Props = {
   featuredRoom: Room;
-}
+};
 
-const FeaturedRoom: FC<Props> = ({ featuredRoom }) => {
+const FeaturedRoom: FC<Props> = props => {
+  const { featuredRoom } = props;
+
   return (
     <section className='flex md:flex-row flex-col px-4 py-10 items-center gap-12 container mx-auto'>
       <div className='md:grid gap-8 grid-cols-1'>
         <div className='rounded-2xl overflow-hidden h-48 mb-4 md:mb-0'>
-          <Image 
+          <Image
             src={featuredRoom.coverImage.url}
             alt={featuredRoom.name}
             width={300}
@@ -25,7 +28,7 @@ const FeaturedRoom: FC<Props> = ({ featuredRoom }) => {
         <div className='grid grid-cols-2 gap-8 h-48'>
           {featuredRoom.images.splice(1, 2).map(image => (
             <div key={image._key} className='rounded-2xl overflow-hidden'>
-              <Image 
+              <Image
                 src={image.url}
                 alt={image._key}
                 width={300}
@@ -42,22 +45,32 @@ const FeaturedRoom: FC<Props> = ({ featuredRoom }) => {
 
         <p className='font-normal max-w-md'>{featuredRoom.description}</p>
 
-          <div className='flex flex-col md:flex-row md:items-end justify-between mt-5'>
-            <div className='flex mb-3 md:mb-0'>
-              <div className='flex gap-3 flex-col items-center justify-center mr-4'>
-                <p className='text-xs lg:text-xl text-center'>Start From</p>
-                <p className='md:font-bold flex font-medium text-lg xl:text-5xl'> <TbCurrencyPeso />{featuredRoom.price}</p>
-              </div>
+        <div className='flex flex-col md:flex-row md:items-end justify-between mt-5'>
+          <div className='flex mb-3 md:mb-0'>
+            <div className='flex gap-3 flex-col items-center justify-center mr-4'>
+              <p className='text-xs lg:text-xl text-center'>Start From</p>
+              <p className='md:font-bold flex font-medium text-lg xl:text-5xl'>
+                $ {featuredRoom.price}
+              </p>
             </div>
-            <Link href={`/rooms${featuredRoom.slug.current}`}
-            className='border h-fit text-center border-tertiary-dark px-3 py-2 lg:py-5 lg:px-7 rounded-lg font-bold lg:text-xl hover:bg-tertiary-light'
+            <div className='flex gap-3 flex-col items-center justify-center mr-4'>
+              <p className='text-xs lg:text-xl text-center'>Discount</p>
+              <p className='md:font-bold flex font-medium text-lg xl:text-5xl'>
+                $ {featuredRoom.discount}
+              </p>
+            </div>
+          </div>
+
+          <Link
+            href={`/rooms/${featuredRoom.slug.current}`}
+            className='border h-fit text-center border-tertiary-dark text-tertiary-dark px-3 py-2 lg:py-5 lg:px-7 rounded-2xl font-bold lg:text-xl'
           >
             More Details
           </Link>
-          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default FeaturedRoom
+export default FeaturedRoom;
