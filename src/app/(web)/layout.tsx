9cbf47d7130d53eb19c4a0f2790e303531f7a73a
TryAgain1122 <luisrafhael1122@gmail.com>
2024-06-01 14:28:10 +0800

    new change file name

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,47 +1,40 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
-
-import Header from '../components/Header/Header';
 import './globals.css';
-import Footer from '../components/Footer/Footer';
-import ThemeProvider from '../components/ThemeProvider/ThemeProvider';
 import { NextAuthProvider } from '../components/AuthProvider/AuthProvider';
 import Toast from '../components/Toast/Toast';
+import ThemeProvider from '../components/ThemeProvider/ThemeProvider'
+import Headers from '../components/Header/Site-Header';
+import Footers from '../components/Footer/Site-Footer';
+
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '700', '900'],
-  style: ['italic', 'normal'],
-  variable: '--font-poppins',
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  style: ["italic", "normal"],
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
-  title: 'Hotel Management App',
-  description: 'Discover the best hotel rooms',
+  title: "urban Deca Tower",
+  description: "Discover the best hotel rooms",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang='en'>
-      <head>
-        <link
-          rel='stylesheet'
-          href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
-          crossOrigin='anonymous'
-        />
-      </head>
+    <html lang="en">
       <body className={poppins.className}>
         <NextAuthProvider>
+          <Toast />
           <ThemeProvider>
-            <Toast />
-            <main className='font-normal'>
-              <Header />
+            <main className="font-normal">
+             <Headers />
               {children}
-              <Footer />
+             <Footers/>
             </main>
           </ThemeProvider>
         </NextAuthProvider>
