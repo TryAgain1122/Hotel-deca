2d1a1cb238d696244233d88c7ca4f468b9520cb6
TryAgain1122 <luisrafhael1122@gmail.com>
2024-07-04 17:11:05 +0800

    new commits

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -2,6 +2,7 @@ import { NextAuthOptions } from 'next-auth';
 import { SanityAdapter, SanityCredentials } from 'next-auth-sanity';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import Auth0Provider from "next-auth/providers/auth0";
 
 import sanityClient from './sanity';
 
@@ -11,10 +12,17 @@ export const authOptions: NextAuthOptions = {
     //   clientId: process.env.GITHUB_CLIENT_ID as string,
     //   clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     // }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID as string,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+      issuer: process.env.AUTH0_ISSUER
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+
+    
     SanityCredentials(sanityClient),
   ],
   session: {
